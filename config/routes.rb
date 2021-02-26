1b5d64015bc2cac89bf0e06ac3df6eb2f1d7369e
sagarakoudai <magnons180@gmail.com>
2021-02-26 18:19:49 +0000

    add:top,index,show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'homes#top'
+
+  get '/index' => 'books#index'
+
+  get '/books/:id' => 'books#show'
 end
