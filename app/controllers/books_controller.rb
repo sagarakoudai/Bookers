527880f4111ace3f87a42103e3486bfacff328ff
sagarakoudai <magnons180@gmail.com>
2021-02-28 15:20:58 +0000

    add successmessage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,8 +20,11 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    if book.update(book_params)
+      redirect_to book_path(book.id), notice: "Book was successfully updated."
+    else
+      render ("/books/edit")
+    end
   end
 
   def destroy
